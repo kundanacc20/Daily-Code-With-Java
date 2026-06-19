1497aeeaa62f1377da810e9a96735a0e11070c0e
kundan kumar <kundankumaracc20@gmail.com>
2026-06-19 07:47:43 +0530

    June:19::2026::07:47:AM

--- src/com/kundan/day19june2026/Main.java
+++ src/com/kundan/day19june2026/Main.java
@@ -4,6 +4,25 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        CircularQueue queue = new CircularQueue(5);
+        try {
+            queue.enqueue(19);
+            queue.enqueue(2);
+            queue.enqueue(3);
+            queue.enqueue(4);
+            queue.enqueue(5);
+
+            System.out.println("Front element: "+queue.peek());
+            System.out.println("Dequeued: "+queue.dequeue());
+
+            queue.enqueue(6);
+
+            while (!queue.isEmpty()){
+                System.out.println("Dequeued: "+queue.dequeue());
+            }
+        } catch (QueueException e){
+            System.out.println("Error: "+e.getMessage());
+        }
 
         //end timern
         long endTimer = System.nanoTime();
