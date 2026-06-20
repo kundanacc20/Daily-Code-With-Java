510fec50c6441e0c5acd72f450d4be8746d7297a
kundan kumar <kundankumaracc20@gmail.com>
2026-06-20 16:34:12 +0530

    June:20::2026::16:34:PM

--- src/com/kundan/day20june2026/QueueLinkedList.java
+++ src/com/kundan/day20june2026/QueueLinkedList.java
@@ -1,4 +1,41 @@
 package com.kundan.day20june2026;
 
 public class QueueLinkedList {
+    private Node front;
+    private Node rear;
+
+    public void enqueue(int data){
+        Node newNode = new Node(data);
+
+        if(rear == null){
+            front = rear = newNode;
+            return;
+        }
+        rear.next = newNode;
+        rear = newNode;
+    }
+
+    public int dequeue() {
+        if (front == null)
+            throw new RuntimeException("queue empty");
+
+            int value = front.data;
+            front = front.next;
+
+            if (front == null) {
+                rear = null;
+            }
+            return value;
+        }
+
+        public int peek () {
+            if (front == null) {
+                throw new RuntimeException("Queue Empty");
+            }
+            return front.data;
+        }
+
+        public boolean isEmpty () {
+            return front == null;
+        }
 }
