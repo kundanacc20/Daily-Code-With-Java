7112c461f389b8cc6d90222cb4bfd6e6f5d98e26
kundan kumar <kundankumaracc20@gmail.com>
2026-06-23 08:23:23 +0530

    June:23::2026::08:23:AM

--- src/com/kundan/day23june2026/Main.java
+++ src/com/kundan/day23june2026/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Person person = new Person("Johnn", 25);
 
+        PersonPrinter printer = new PersonPrinter();
+        printer.printPerson(person);
         //end timer
         long endTimer = System.nanoTime();
 
         //program time
-        long programTime = System.nanoTime();
+        long programTime = (endTimer - startTimer)/1_000_000;
 
         System.out.println("program time: "+programTime+" ms");
     }
--- src/com/kundan/day23june2026/PersonPrinter.java
+++ src/com/kundan/day23june2026/PersonPrinter.java
@@ -1,5 +1,11 @@
 package com.kundan.day23june2026;
 
 public class PersonPrinter {
+    public void printPerson(Person person){
+        String name = person.name();
+        int age = person.age();
 
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
 }
