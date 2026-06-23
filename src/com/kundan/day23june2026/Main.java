954f775b9f1221366adca2e9962c5acaa1549b87
kundan kumar <kundankumaracc20@gmail.com>
2026-06-23 08:30:15 +0530

    June:23::2026::08:30:AM

--- src/com/kundan/day23june2026/Main.java
+++ src/com/kundan/day23june2026/Main.java
@@ -4,10 +4,19 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        Person person = new Person("Johnn", 25);
+//        Person person = new Person("Johnn", 25);
+//
+//        PersonPrinter printer = new PersonPrinter();
+//        printer.printPerson(person);
 
-        PersonPrinter printer = new PersonPrinter();
-        printer.printPerson(person);
+        //Using Record Patterns
+
+        Object obj = new Person("John", 25);
+
+        if(obj instanceof Person(String name,int age)){
+            System.out.println("Name: "+name);
+            System.out.println("Age: "+age);
+        }
         //end timer
         long endTimer = System.nanoTime();
 
