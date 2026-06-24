1a1573e2060a0fd93b5ffeb9cd3cef77e21eb61d
kundan kumar <kundankumaracc20@gmail.com>
2026-06-24 08:43:00 +0530

    June:24::2026::08:43:AM

--- src/com/kundan/day24june2026/Main.java
+++ src/com/kundan/day24june2026/Main.java
@@ -1,22 +1,36 @@
 package com.kundan.day24june2026;
 
+record Person(String name, int age){}
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        Employee emp = new Employee("john",30,
-                new Address("Ahmedabad","gujrat"));
+//        Employee emp = new Employee("john",30,
+//                new Address("Ahmedabad","gujrat"));
+//
+//        if(emp instanceof Employee(
+//                String name,
+//                int age,
+//                Address(String city,
+//                        String state)
+//        )){
+//            System.out.println(name);
+//            System.out.println(age);
+//            System.out.println(city);
+//            System.out.println(state);
+//        }
 
-        if(emp instanceof Employee(
-                String name,
-                int age,
-                Address(String city,
-                        String state)
-        )){
-            System.out.println(name);
-            System.out.println(age);
-            System.out.println(city);
-            System.out.println(state);
+        Object obj = new Person("john", 25);
+
+        switch (obj){
+            case Person(String name, int age) ->
+                System.out.println(
+                        "Name: "+name+
+                                ", Age: "+age
+                );
+            default ->
+                System.out.println("Unknown");
         }
         //end timer
         long endTimer = System.nanoTime();
