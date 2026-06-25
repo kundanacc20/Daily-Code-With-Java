6140b1d0dfa4ff30aa7cf574413b22b34fb3d554
kundan kumar <kundankumaracc20@gmail.com>
2026-06-25 08:59:05 +0530

    June:25::2026::08:59:AM

--- src/com/kundan/day25june2026/Main.java
+++ src/com/kundan/day25june2026/Main.java
@@ -4,6 +4,19 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        //without record pattern
+        Order order = new Order(
+                101,
+                new Customer(1,"kundan"),
+                50000
+        );
+
+        Customer c = order.customer();
+
+        System.out.println(order.orderId());
+        System.out.println(c.id());
+        System.out.println(c.name());
+        System.out.println(order.amount());
 
         //end timer
         long endTimer = System.nanoTime();
