ae8c66683f1108c75b6fd4ba2aa70ebcdd8935fe
kundan kumar <kundankumaracc20@gmail.com>
2026-06-25 09:05:20 +0530

    June:25::2026::09:05:AM

--- src/com/kundan/day25june2026/Main.java
+++ src/com/kundan/day25june2026/Main.java
@@ -4,19 +4,40 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        //without record pattern
+
+//        //without record pattern
+//        Order order = new Order(
+//                101,
+//                new Customer(1,"kundan"),
+//                50000
+//        );
+//
+//        Customer c = order.customer();
+//
+//        System.out.println(order.orderId());
+//        System.out.println(c.id());
+//        System.out.println(c.name());
+//        System.out.println(order.amount());
+
+        //With Record Patterns
+
         Order order = new Order(
                 101,
-                new Customer(1,"kundan"),
-                50000
+                new Customer(1, "kumar"),
+                60000
         );
 
-        Customer c = order.customer();
-
-        System.out.println(order.orderId());
-        System.out.println(c.id());
-        System.out.println(c.name());
-        System.out.println(order.amount());
+        if(order instanceof Order(
+                int orderId,
+                Customer(int customerId,
+                         String customerName),
+                double amount
+        )){
+            System.out.println(orderId);
+            System.out.println(customerId);
+            System.out.println(customerName);
+            System.out.println(amount);
+        }
 
         //end timer
         long endTimer = System.nanoTime();
