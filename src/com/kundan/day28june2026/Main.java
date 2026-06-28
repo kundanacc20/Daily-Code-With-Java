ce752f81884a385799551faec59e6b8ed2cbf34e
kundan kumar <kundankumaracc20@gmail.com>
2026-06-28 14:16:08 +0530

    June:28::2026::14:16:PM

--- src/com/kundan/day28june2026/Main.java
+++ src/com/kundan/day28june2026/Main.java
@@ -1,10 +1,22 @@
 package com.kundan.day28june2026;
 
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
+        pq.offer(20);
+        pq.offer(5);
+        pq.offer(10);
+        pq.offer(30);
+
+        while (!pq.isEmpty()){
+            System.out.print(pq.poll()+" ");
+        }
+        System.out.println();
         //end timer
         long endTimer = System.nanoTime();
 
