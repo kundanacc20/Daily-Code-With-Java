a3815e5a51c547d853eba78ef6174158c18cfa5e
kundan kumar <kundankumaracc20@gmail.com>
2026-06-29 08:47:01 +0530

    June:29::2026::08:47:AM

--- src/com/kundan/day29june2026/CircuitBreaker.java
+++ src/com/kundan/day29june2026/CircuitBreaker.java
@@ -1,4 +1,55 @@
 package com.kundan.day29june2026;
 
 public class CircuitBreaker {
+    private CircuitBreakerState state = CircuitBreakerState.CLOSED;
+
+    private final int failureThreshold;
+    private final long retryTimePeriod;
+
+    private int failureCount =0;
+    private long lastFailureTime =0;
+
+    public CircuitBreaker(int failureThreshold, long retryTimePeriod){
+        this.failureThreshold = failureThreshold;
+        this.retryTimePeriod = retryTimePeriod;
+    }
+
+    public String execute(ExternalService service){
+        if(state == CircuitBreakerState.OPEN){
+            if(System.currentTimeMillis() - lastFailureTime >= retryTimePeriod){
+                state = CircuitBreakerState.HALF_OPEN;
+                System.out.println("circuit moved to HALF_OPEN");
+            } else {
+                return "Request Blocked (Circuit OPEN)";
+            }
+        }
+        try {
+            String response = service.callService();
+            reset();
+
+            return response;
+        } catch (Exception e){
+            recordFailure();
+            return e.getMessage();
+        }
+    }
+    private void recordFailure(){
+        failureCount++;
+
+        lastFailureTime = System.currentTimeMillis();
+        System.out.println("Failure count: "+failureCount);
+
+        if(failureCount >= failureThreshold){
+            state = CircuitBreakerState.OPEN;
+            System.out.println("circuit moved to open");
+        }
+    }
+    private void reset(){
+        failureCount =0;
+        state = CircuitBreakerState.CLOSED;
+        System.out.println("circuit move to closed ");
+    }
+    public CircuitBreakerState getState(){
+        return state;
+    }
 }
--- src/com/kundan/day29june2026/Main.java
+++ src/com/kundan/day29june2026/Main.java
@@ -1,9 +1,23 @@
 package com.kundan.day29june2026;
 
 public class Main {
-    public static void main(String[] args){
-     //start timer
-     long startTimer = System.nanoTime();
+    public static void main(String[] args) throws Exception {
+        //start timer
+        long startTimer = System.nanoTime();
+
+        ExternalService service = new ExternalService();
+
+        CircuitBreaker breaker = new CircuitBreaker(3, 5000);
+
+        for(int i= 1; i <= 20; i++){
+            System.out.println("-------------------------------");
+            System.out.println("Request: "+i);
+            String result = breaker.execute(service);
+            System.out.println(result);
+
+            System.out.println("Circuit State: "+breaker.getState());
+            Thread.sleep(1000);
+        }
 
      //end timer
      long endTimer = System.nanoTime();
