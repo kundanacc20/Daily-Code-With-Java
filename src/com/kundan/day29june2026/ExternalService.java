7052f611f8d1069dadea69a3a3e57ac47058d788
kundan kumar <kundankumaracc20@gmail.com>
2026-06-29 08:22:20 +0530

    June:29::2026::08:22:AM

--- src/com/kundan/day29june2026/ExternalService.java
+++ src/com/kundan/day29june2026/ExternalService.java
@@ -1,4 +1,14 @@
 package com.kundan.day29june2026;
 
+import java.util.Random;
+
 public class ExternalService {
+    private final Random random = new Random();
+
+    public String callService(){
+        if(random.nextInt(10)<7){
+            throw new RuntimeException("External Service Failed");
+        }
+        return "External service responser";
+    }
 }
