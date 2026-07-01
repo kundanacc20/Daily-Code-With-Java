8e904789f36241c76824d0a5bdcad1996f35c14e
kundan kumar <kundankumaracc20@gmail.com>
2026-07-01 07:46:24 +0530

    July:01::2026::07:46:AM

--- src/com/kundan/day01july2026/LoadBalancer.java
+++ src/com/kundan/day01july2026/LoadBalancer.java
@@ -1,4 +1,27 @@
 package com.kundan.day01july2026;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class LoadBalancer {
+    private final List<Server> servers;
+    private final AtomicInteger counter;
+
+    public LoadBalancer(List<Server> servers){
+        if(servers == null || servers.isEmpty()){
+            throw new IllegalArgumentException("server list cannot be empty");
+        }
+        this.servers = servers;
+        this.counter = new AtomicInteger(0);
+    }
+
+    public Server getServer(){
+        int index = Math.abs(counter.getAndIncrement()%servers.size());
+        return servers.get(index);
+    }
+
+    public void handleRequest(String request){
+        Server server = getServer();
+        server.processRequest(request);
+    }
 }
