af518d38fe1387c0a7f04d564734ee18f0045014
kundan kumar <kundankumaracc20@gmail.com>
2026-07-01 07:53:42 +0530

    July:01::2026::07:53:AM

--- src/com/kundan/day01july2026/Main.java
+++ src/com/kundan/day01july2026/Main.java
@@ -1,10 +1,22 @@
 package com.kundan.day01july2026;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        List<Server> servers = List.of(
+                new Server("server-1"),
+                new Server("Server-2"),
+                new Server("Server-3")
+        );
 
+        LoadBalancer loadBalancer = new LoadBalancer(servers);
+
+        for(int i =1; i<=10; i++){
+            loadBalancer.handleRequest("Request-"+i);
+        }
         //end timer
         long endTimer = System.nanoTime();
 
