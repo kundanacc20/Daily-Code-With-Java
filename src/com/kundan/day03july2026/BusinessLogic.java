cdcd10f37f681ee9f317f2aec7383956b06fd935
kundan kumar <kundankumaracc20@gmail.com>
2026-07-03 06:18:15 +0530

    July:03::2026::06:18:AM

--- src/com/kundan/day03july2026/BusinessLogic.java
+++
@@ -1,4 +0,0 @@
-package com.kundan.day03july2026;
-
-public class BusinessLogic {
-}
--- src/com/kundan/day03july2026/Main.java
+++ src/com/kundan/day03july2026/Main.java
@@ -1,10 +1,15 @@
 package com.kundan.day03july2026;
 
+
 public class Main {
+
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-
+        Printer.printLength("hello");
+        Printer.printLength(42);
+        Printer.printLength(java.util.List.of(1,2,3));
+        Printer.printLength(3.14);
         //end timer
         long endTimer = System.nanoTime();
 
