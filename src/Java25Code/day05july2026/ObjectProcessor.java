c92da3a0e3ca95226db59b0eafeb98aa18474458
kundan kumar <kundankumaracc20@gmail.com>
2026-07-05 11:26:02 +0530

    July:05::2026::11:26:AM

--- src/Java25Code/day05july2026/ObjectProcessor.java
+++ src/Java25Code/day05july2026/ObjectProcessor.java
@@ -1,15 +1,26 @@
 package Java25Code.day05july2026;
 
 public class ObjectProcessor {
-    public String processObject(Object ob){
-        if(ob instanceof String){
-            return new StringHandler().handle((String) ob);
-        } else if(ob instanceof Integer){
-            return new IntegerHandler().handle((Integer) ob);
-        } else if(ob instanceof Double){
-            return new DoubleHandler().handle((Double) ob);
-        } else {
-            return "Unknown type";
-        }
+//    public String processObject(Object ob){
+//        if(ob instanceof String){
+//            return new StringHandler().handle((String) ob);
+//        } else if(ob instanceof Integer){
+//            return new IntegerHandler().handle((Integer) ob);
+//        } else if(ob instanceof Double){
+//            return new DoubleHandler().handle((Double) ob);
+//        } else {
+//            return "Unknown type";
+//        }
+//    }
+
+    //With Java 25 Pattern Matching
+    public String processObject(Object obj){
+        return switch (obj) {
+            case String s -> new StringHandler().handle(s);
+            case Integer i -> new IntegerHandler().handle(i);
+            case Double d -> new DoubleHandler().handle(d);
+            default -> "unknown type";
+        };
+
     }
 }
