4445ddc1d52a2f9ef7715284940911d78d2c4e70
kundan kumar <kundankumaracc20@gmail.com>
2026-07-06 09:41:13 +0530

    July:06::2026::09:41:AM

--- src/Java25Code/day06july2026/Main.java
+++ src/Java25Code/day06july2026/Main.java
@@ -4,7 +4,27 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        ApiResponseHandler handler = new ApiResponseHandler();
 
+        //admin user login
+        UserData admin = new UserData("1","amin@example.com","ADMIN");
+        handler.handleUserResponse(new ApiResponse.Success<>(admin));
+
+        //regular user login
+        UserData user = new UserData("2", "user@example.com","USER");
+        handler.handleUserResponse(new ApiResponse.Success<>(user));
+
+        //unauthorized error
+        handler.handleUserResponse(new ApiResponse.Error<>(401, "Invalid token"));
+
+        //server error
+        handler.handleUserResponse(new ApiResponse.Error<>(500,"Database down"));
+
+        //client error
+        handler.handleUserResponse(new ApiResponse.Error<>(404,"user not found"));
+
+        //pending response
+        handler.handleUserResponse(new ApiResponse.Pending<>());
         //end timer
         long endTimer = System.nanoTime();
 
