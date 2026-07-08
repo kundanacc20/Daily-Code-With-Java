87bf32492c05e37125fcd51ac12897b82f1eec34
kundan kumar <kundankumaracc20@gmail.com>
2026-07-08 07:24:39 +0530

    July:08::2026::07:24:AM

---
+++ src/Java25Code/day08july2026/Employee.java
@@ -0,0 +1,12 @@
+package Java25Code.day08july2026;
+
+public class Employee {
+    private int id;
+
+    public Employee(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+}
