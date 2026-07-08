9c42ff5510bac250a7074caf7a7b2ad871f31d31
kundan kumar <kundankumaracc20@gmail.com>
2026-07-08 07:39:02 +0530

    July:08::2026::07:39:AM

--- src/Java25Code/day08july2026/Main.java
+++ src/Java25Code/day08july2026/Main.java
@@ -1,10 +1,17 @@
 package Java25Code.day08july2026;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         //start timer
         long startTimer = System.nanoTime();
+        PermanentEmployeeManager manager = new PermanentEmployeeManager();
+        manager.loadEmployees(1000);
 
+        TemporaryObjectGenerator generator = new TemporaryObjectGenerator();
+        generator.generateTemporaryObjects(1_000_000);
+
+        System.out.println("Permanent employees loaded: "+manager.getEmployees().size());
+        Thread.sleep(10000);
         //end timer
         long endTimer = System.nanoTime();
 
