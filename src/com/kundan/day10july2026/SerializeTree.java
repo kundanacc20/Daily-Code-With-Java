dc6998b3e7a51f496bbe3e9ad60f5353d64f3f50
kundan kumar <kundankumaracc20@gmail.com>
2026-07-10 09:48:41 +0530

    July:10::2026::09:48:AM

--- src/com/kundan/day10july2026/SerializeTree.java
+++ src/com/kundan/day10july2026/SerializeTree.java
@@ -1,4 +1,28 @@
 package com.kundan.day10july2026;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SerializeTree {
+    public String serialize(TreeNode root){
+        if(root == null)
+            return "";
+
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+
+            if(node == null){
+                sb.append("null, ");
+            } else {
+                sb.append(node.val).append(",");
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+        return sb.toString();
+    }
 }
