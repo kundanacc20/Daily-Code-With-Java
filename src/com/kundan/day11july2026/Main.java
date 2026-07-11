9ff580e737aa59538d50d62692cdc12a79a1b45e
kundan kumar <kundankumaracc20@gmail.com>
2026-07-11 21:23:09 +0530

    July:11::2026::21:23:PM

--- src/com/kundan/day11july2026/Main.java
+++ src/com/kundan/day11july2026/Main.java
@@ -6,10 +6,15 @@ public class Main {
         long startTimer = System.nanoTime();
 //        Calculator c = (a,b) ->a+b;
 //        System.out.println(c.add(10,20));
+//        //Predicate<T>
+//
+//        Predicate<Integer> isEven = n -> n % 2 == 0;
+//        System.out.println(isEven.test(10));
+//        System.out.println(isEven.test(15));
 
-        Predicate<Integer> isEven = n -> n % 2 == 0;
-        System.out.println(isEven.test(10));
-        System.out.println(isEven.test(15));
+        //Function<T,R>
+        Function<String, Integer> lengthFinder = s -> s.length();
+        System.out.println(lengthFinder.apply("java"));
         //end timer
         long endTimer = System.nanoTime();
 
