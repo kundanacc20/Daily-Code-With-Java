6d8482f9361dd8e121265714f0b4ede0f974473c
kundan kumar <kundankumaracc20@gmail.com>
2026-07-12 13:21:10 +0530

    July:12::2026::13:21:PM

---
+++ src/com/kundan/day12july2026/Calculator.java
@@ -0,0 +1,7 @@
+package com.kundan.day12july2026;
+
+public class Calculator {
+    public static double calculate() {
+        return 10.0/3;
+    }
+}
---
+++ src/com/kundan/day12july2026/Main.java
@@ -0,0 +1,17 @@
+package com.kundan.day12july2026;
+
+public class Main {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+        //Feature 1: Always-strict floating point semantics
+        System.out.println(Calculator.calculate());
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer - startTimer)/1_000_000;
+
+        System.out.println("Program Time: "+programTime+" ms");
+    }
+}
