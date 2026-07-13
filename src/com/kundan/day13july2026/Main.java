9fbfcf1aa4ed2e2965ac80031e655b07087bac36
kundan kumar <kundankumaracc20@gmail.com>
2026-07-13 09:00:30 +0530

    july:13::2026::09:00:AM

--- src/com/kundan/day13july2026/Main.java
+++ src/com/kundan/day13july2026/Main.java
@@ -1,13 +1,23 @@
 package com.kundan.day13july2026;
 
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
         RandomService randomService = new RandomService();
 
+        RandomGenerator generator = RandomGeneratorFactory.of("L64X128MixRandom").create();
+
+
         System.out.println("Random Integer(0 - 99): "+randomService.getRandomInt(100));
         System.out.println("Random Double(0 - 1): "+randomService.getRandomDouble());
+
+        System.out.println("--------------Specific Generator-------------------");
+        System.out.println(generator.nextInt(100));
+
 
         //end timer
         long endTimer = System.nanoTime();
