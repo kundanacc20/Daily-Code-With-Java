3218cd70ad328f079c30b6c3dca46593cd152007
kundan kumar <kundankumaracc20@gmail.com>
2026-07-15 08:41:26 +0530

    July:15::2026::08:41:AM

--- src/java17code/day15july2026/Main.java
+++ src/java17code/day15july2026/Main.java
@@ -1,10 +1,31 @@
 package java17code.day15july2026;
 
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        try {
+            Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+            unsafeField.setAccessible(true);
 
+            Unsafe unsafe = (Unsafe) unsafeField.get(null);
+
+            System.out.println("Unsafe instance obtained: "+unsafe);
+            System.out.println("This should not print in java 17 without -- add-opens");
+
+        } catch (NoSuchFieldException e){
+            System.out.println("Field not found: "+e.getMessage());
+        } catch (IllegalAccessException e){
+            System.out.println("Illegal access: "+e.getMessage());
+        } catch (Exception e){
+            System.out.println("Error : "+e.getClass().getName());
+            System.out.println("Messgae: "+e.getMessage());
+            e.printStackTrace();
+        }
         //end timer
         long endTimer = System.nanoTime();
 
