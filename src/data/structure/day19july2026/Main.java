d7ae5348dcc823128c9f0d28d615d2da690b4aeb
kundan kumar <kundankumaracc20@gmail.com>
2026-07-19 21:43:05 +0530

    July:19::2026::21:43:PM

---
+++ src/data/structure/day19july2026/Main.java
@@ -0,0 +1,15 @@
+package data.structure.day19july2026;
+
+public class Main {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer - startTimer)/1_000_000;
+        System.out.println("Program time :"+programTime+" ms");
+    }
+}
--- src/java17code/day15july2026/Main.java
+++ src/java17code/day15july2026/Main.java
@@ -1,5 +1,6 @@
 package java17code.day15july2026;
 
+import com.kundan.day26june2026.Shape;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
@@ -8,24 +9,24 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        try {
-            Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
-            unsafeField.setAccessible(true);
-
-            Unsafe unsafe = (Unsafe) unsafeField.get(null);
-
-            System.out.println("Unsafe instance obtained: "+unsafe);
-            System.out.println("This should not print in java 17 without -- add-opens");
-
-        } catch (NoSuchFieldException e){
-            System.out.println("Field not found: "+e.getMessage());
-        } catch (IllegalAccessException e){
-            System.out.println("Illegal access: "+e.getMessage());
-        } catch (Exception e){
-            System.out.println("Error : "+e.getClass().getName());
-            System.out.println("Messgae: "+e.getMessage());
-            e.printStackTrace();
-        }
+//        try {
+//            Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+//            unsafeField.setAccessible(true);
+//
+//            Unsafe unsafe = (Unsafe) unsafeField.get(null);
+//
+//            System.out.println("Unsafe instance obtained: "+unsafe);
+//            System.out.println("This should not print in java 17 without -- add-opens");
+//
+//        } catch (NoSuchFieldException e){
+//            System.out.println("Field not found: "+e.getMessage());
+//        } catch (IllegalAccessException e){
+//            System.out.println("Illegal access: "+e.getMessage());
+//        } catch (Exception e){
+//            System.out.println("Error : "+e.getClass().getName());
+//            System.out.println("Messgae: "+e.getMessage());
+//            e.printStackTrace();
+//        }
         //end timer
         long endTimer = System.nanoTime();
 
@@ -34,4 +35,5 @@ public class Main {
 
         System.out.println("Program time: "+programTime+" ms");
     }
+
 }
