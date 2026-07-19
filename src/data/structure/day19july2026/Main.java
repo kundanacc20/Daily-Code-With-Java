162c569f17473184efebf5a44d732aa7e79c8258
kundan kumar <kundankumaracc20@gmail.com>
2026-07-19 21:54:20 +0530

    July:19::2026::21:54:PM

--- src/data/structure/day19july2026/Main.java
+++ src/data/structure/day19july2026/Main.java
@@ -1,10 +1,25 @@
 package data.structure.day19july2026;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        //Max Heap Using PriorityQueue
 
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(
+                Collections.reverseOrder());
+
+        maxHeap.offer(20);
+        maxHeap.offer(4);
+        maxHeap.offer(50);
+        maxHeap.offer(10);
+
+        while (!maxHeap.isEmpty()){
+            System.out.println(maxHeap.poll());
+        }
         //end timer
         long endTimer = System.nanoTime();
 
