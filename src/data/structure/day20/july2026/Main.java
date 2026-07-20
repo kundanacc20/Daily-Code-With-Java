fd3c11940de1f36e28ee409eb57bfbca9d826769
kundan kumar <kundankumaracc20@gmail.com>
2026-07-20 10:18:40 +0530

    July:20::2026::10:18:AM

--- src/data/structure/day20/july2026/Main.java
+++ src/data/structure/day20/july2026/Main.java
@@ -1,10 +1,17 @@
 package data.structure.day20.july2026;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        TwoSum obj = new TwoSum();
+        int[] nums = {2,7,11,15};
+        int target = 9;
 
+        int[] result = TwoSum.twoSum(nums,target);
+        System.out.println(Arrays.toString(result));
         //end timer
         long endTimer = System.nanoTime();
 
