e8d11dc3aa9259d9e2170081c257d9cc623c9d67
kundan kumar <kundankumaracc20@gmail.com>
2026-07-20 10:12:00 +0530

    July:20::2026::10:11:AM

--- src/data/structure/day20/july2026/Main.java
+++ src/data/structure/day20/july2026/Main.java
@@ -2,6 +2,15 @@ package data.structure.day20.july2026;
 
 public class Main {
     public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
 
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer-startTimer)/1_000_000;
+
+        System.out.println("program time:  "+programTime+" ms");
     }
 }
