f8fc109f0d08bb4669a3752d7d4870dc6824d4ec
kundan kumar <kundankumaracc20@gmail.com>
2026-07-22 09:22:35 +0530

    July:22:2026::09:22:AM

--- src/data/structure/day22july2026/Main.java
+++ src/data/structure/day22july2026/Main.java
@@ -4,7 +4,25 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        //create first sorted list
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
 
+        //create second sorted list
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        //merge the list
+        MergeList merger = new MergeList();
+        ListNode mergerdHead = merger.merge(l1,l2);
+
+        //print merged list
+        while(mergerdHead != null){
+            System.out.print(mergerdHead.val + " ");
+            mergerdHead = mergerdHead.next;
+        }
         //end timer
         long endTimer = System.nanoTime();
 
