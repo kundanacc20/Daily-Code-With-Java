2cf1cb3390e37cb77413bd0fb767b458f6b98f0e
kundan kumar <kundankumaracc20@gmail.com>
2026-07-23 09:04:13 +0530

    July:23::2026::09:04:AM

--- src/data/structure/day23july2026/Main.java
+++ src/data/structure/day23july2026/Main.java
@@ -4,7 +4,9 @@ public class Main {
  public static void main(String[] args){
      //start timer
      long startTimer = System.nanoTime();
-
+     String input = "abcabbb";
+     int result = SubstringUtil.lengthOfLongestSubstring(input);
+     System.out.println("Longest substring length: "+result);
      //end timer
      long endTimer = System.nanoTime();
 
