a175aa10184f1c42a85e4cb51273bc05740273df
kundan kumar <kundankumaracc20@gmail.com>
2026-07-24 09:35:22 +0530

    July:24::2026::09:35:AM

--- src/data/structure/day24july2026/Main.java
+++ src/data/structure/day24july2026/Main.java
@@ -4,7 +4,28 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        //build sample tree
+        TreeNode root = new TreeNode(3);
 
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+
+        // Create LCA object
+        LCA lcaFinder = new LCA();
+
+        // Find LCA of nodes 5 and 1
+        TreeNode ancestor = lcaFinder.lowestCommonAncestor(root, root.left, root.right);
+        System.out.println("LCA of 5 and 1: " + ancestor.val);
+
+        // Find LCA of nodes 6 and 4
+        TreeNode ancestor2 = lcaFinder.lowestCommonAncestor(root, root.left.left, root.left.right.right);
+        System.out.println("LCA of 6 and 4: " + ancestor2.val);
         //end timer
         long endTimer = System.nanoTime();
 
--- src/data/structure/day24july2026/TreeNode.java
+++ src/data/structure/day24july2026/TreeNode.java
@@ -1,4 +1,12 @@
 package data.structure.day24july2026;
 
 public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    public TreeNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
