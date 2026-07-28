c760e90910e883cb7e2cb124b73c8ecc9ee0b1b7
kundan kumar <kundankumaracc20@gmail.com>
2026-07-28 09:07:02 +0530

    July:28::2026::09:07:AM

--- src/data/structure/day28july2026/GridInput.java
+++ src/data/structure/day28july2026/GridInput.java
@@ -1,4 +1,13 @@
 package data.structure.day28july2026;
 
 public class GridInput {
+
+    public static char[][] sampleGrid(){
+        return new char[][]{
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+    }
 }
