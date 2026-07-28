82f46f2de84c62c5f27cf759ac94d58fc532eb12
kundan kumar <kundankumaracc20@gmail.com>
2026-07-28 09:09:40 +0530

    July:28::2026::09:09:AM

--- src/data/structure/day28july2026/Main.java
+++ src/data/structure/day28july2026/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        char[][] grid = GridInput.sampleGrid();
 
+        NumberOfIslands solver = new NumberOfIslands();
+        int result = solver.numIslands(grid);
+
+        System.out.println("Number of Islands: "+result);
         //end timer
         long endTimer = System.nanoTime();
 
