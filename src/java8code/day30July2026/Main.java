9793c5d9aec705d5fd4cf737c15032ba502826f4
kundan kumar <kundankumaracc20@gmail.com>
2026-07-30 09:11:00 +0530

    June:30::2026::09:11:AM

--- src/java8code/day30July2026/Main.java
+++ src/java8code/day30July2026/Main.java
@@ -2,18 +2,31 @@ package java8code.day30July2026;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        //1. Count Strings Starting With "A"
-        List<String> words = Arrays.asList("Apple","Among","Anil","Orange","banana");
+//        //1. Count Strings Starting With "A"
+//        List<String> words = Arrays.asList("Apple","Among","Anil","Orange","banana");
+//
+//        long finalCount = words.stream().filter(n ->n.startsWith("A") || n.startsWith("b"))
+//                .count();
+//        words.stream().filter(n ->n.startsWith("A") || n.startsWith("b"))
+//                .forEach(System.out::println);
+//
+//        System.out.println(finalCount);
 
-        long finalCount = words.stream().filter(n ->n.startsWith("A") || n.startsWith("b"))
-                .count();
+        //2. Find Maximum Number and Minimum number
 
-        System.out.println(finalCount);
+        List<Integer> numbers = Arrays.asList(1,2,99,189,333,999);
+
+        Optional<Integer> maxNumber= numbers.stream().max(Integer::compareTo);
+        System.out.println(maxNumber);
+
+        Optional<Integer> minNumber = numbers.stream().min(Integer::compareTo);
+        System.out.println(minNumber);
         //end timer
         long endTimer = System.nanoTime();
 
