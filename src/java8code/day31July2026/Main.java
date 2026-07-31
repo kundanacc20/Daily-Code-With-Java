135cd3d055cb64c6e862cbf9f8ea19018f0be26f
kundan kumar <kundankumaracc20@gmail.com>
2026-07-31 09:27:41 +0530

    July:31::2026::09:27:AM

--- src/java8code/day31July2026/Main.java
+++ src/java8code/day31July2026/Main.java
@@ -2,13 +2,15 @@ package java8code.day31July2026;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.OptionalLong;
 
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
 //        1. Calculate Sum of All Numbers
-        List<Integer> numbers = Arrays.asList(1,2,5,4,7,9,10);
+//        List<Integer> numbers = Arrays.asList(1,2,5,4,7,9,10);
 
 //        long finalSum = numbers.stream()
 //                .reduce((a,b) ->a+b)
@@ -22,12 +24,32 @@ public class Main {
 //        System.out.println(sum);
 //        System.out.println(((Object) sum).getClass().getName());
 
-        long finalSum = numbers.stream()
-                .mapToLong(Integer::longValue)
-                .sum();
+//        long finalSum = numbers.stream()
+//                .mapToLong(Integer::longValue)
+//                .sum();
+//
+//        System.out.println(finalSum);
+//        System.out.println(((Object) finalSum).getClass().getName());
 
-        System.out.println(finalSum);
-        System.out.println(((Object) finalSum).getClass().getName());
+//        2. Find Average
+
+//        List<Integer> numberList = Arrays.asList(1,5,2,9,3);
+//
+//        OptionalDouble averageValue = numberList.stream()
+//                .mapToInt(Integer::intValue)
+//                .average();
+//
+//        System.out.println(averageValue);
+
+//        List<Double> avgDouble = Arrays.asList(3.0,2.0,5.0,9.0);
+//        double avgFinalDouble = avgDouble.stream()
+//                .mapToDouble(Double::doubleValue)
+//                .average()
+//                .orElse(0.0);
+//
+//        System.out.println(avgFinalDouble);
+
+
         //end timer
         long endTimer = System.nanoTime();
 
