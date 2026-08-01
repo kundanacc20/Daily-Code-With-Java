abf96359c1333f3aec4d7fa141e1d3c5f3f40dd2
kundan kumar <kundankumaracc20@gmail.com>
2026-08-01 21:51:06 +0530

    August:01::2026::21:51:PM

--- src/java8code/day01August2026/Main.java
+++ src/java8code/day01August2026/Main.java
@@ -1,10 +1,20 @@
 package java8code.day01August2026;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        List<Employee> empList = Arrays.asList(new Employee(100,"kundan",100000),
+                new Employee(101,"harshit",100000),
+                new Employee(102,"harishankar",100000));
 
+        List<Employee> finalList = empList.stream()
+                .sorted((e1,e2) ->Integer.compare(e1.getId(), e2.getId())).toList();
+        System.out.println(finalList);
         //end timer
         long endTimer = System.nanoTime();
 
