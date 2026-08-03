c1808fb0e928ef7ccd18230d1c40c021c86b7b44
kundan kumar <kundankumaracc20@gmail.com>
2026-08-03 09:46:27 +0530

    August:03::2026::09:46:AM

---
+++ src/java8code/day03August2026/Employee.java
@@ -0,0 +1,28 @@
+package java8code.day03August2026;
+
+public class Employee {
+    private int id;
+    private String name;
+    private long salary;
+
+    public Employee(int id, String name, long salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public long getSalary(){
+        return salary;
+    }
+
+    public String toString(){
+        return "id "+id+ " name: "+name+" salary"+salary;
+    }
+}
---
+++ src/java8code/day03August2026/Main.java
@@ -0,0 +1,17 @@
+package java8code.day03August2026;
+
+public class Main {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+//        1. Find Highest Salary Employee
+
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer - startTimer)/1_000_000;
+
+        System.out.println("Program Time: "+programTime+ " ms");
+    }
+}
