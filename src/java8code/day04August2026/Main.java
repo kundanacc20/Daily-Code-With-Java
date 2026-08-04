9c552f407941180b99b7d85dbced67476c5406d7
kundan kumar <kundankumaracc20@gmail.com>
2026-08-04 08:44:28 +0530

    August:04::2026::08::44:AM

--- src/java8code/day04August2026/Main.java
+++ src/java8code/day04August2026/Main.java
@@ -2,18 +2,25 @@ package java8code.day04August2026;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
 
-        //1. Calculate Sum of All Numbers
-        List<Integer> numList = Arrays.asList(1,9,2,6,1,9,7);
+//        //1. Calculate Sum of All Numbers
+//        List<Integer> numList = Arrays.asList(1,9,2,6,1,9,7);
+//
+//        long sum = numList.stream().reduce(0,Integer::sum);
 
-        long sum = numList.stream().reduce(0,Integer::sum);
+//        System.out.println(sum);
 
-        System.out.println(sum);
+        //2. finding the max element in the list
+        List<Integer> numbers = Arrays.asList(1,2,6,8,3,0);
+
+         Optional<Integer> maxElement = numbers.stream().reduce(Integer::max);
+         System.out.println(maxElement.get());
         //end timer
         long endTimer = System.nanoTime();
 
