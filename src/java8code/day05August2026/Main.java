1343bbccbde3a664e87137521c58363902763b6e
kundan kumar <kundankumaracc20@gmail.com>
2026-08-05 10:07:19 +0530

    August:05::2026::10:07:AM

---
+++ src/java8code/day05August2026/Main.java
@@ -0,0 +1,30 @@
+package java8code.day05August2026;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+        //1. Group Employees by Department
+        List<Employees> emplst = Arrays.asList(
+                new Employees("kundan",1000000,"developer"),
+                new Employees("kumar",100009,"developer"),
+                new Employees("harsheet",100008, "marketing"));
+        Map<String,List<Employees>> finalEmpList = emplst.stream()
+                .collect(Collectors.groupingBy(Employees::getDept));
+
+        System.out.println(finalEmpList);
+
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer -startTimer)/(1_000_000);
+
+        System.out.println("program time: "+programTime+ " ms");
+    }
+}
