785c4759ad71a7837189304df12ede911389e5aa
kundan kumar <kundankumaracc20@gmail.com>
2026-08-06 09:55:25 +0530

    August:06::2026::09:55:AM

--- src/java8code/day06August2026/Employee.java
+++ src/java8code/day06August2026/Employee.java
@@ -2,8 +2,8 @@ package java8code.day06August2026;
 
 public record Employee(int id, String name, String dept) {
 
-    @Override
-    public String toString(){
-        return "id : "+id+ " name: "+name+" Dept: "+dept;
-    }
+//    @Override
+//    public String toString(){
+//        return "id : "+id+ " name: "+name+" Dept: "+dept;
+//    }
 }
