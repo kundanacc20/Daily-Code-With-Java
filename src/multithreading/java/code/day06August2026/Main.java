9a3f78c034de3de949a99870dd36bc1de1f383b4
kundan kumar <kundankumaracc20@gmail.com>
2026-08-06 08:38:18 +0530

    multithreading code

--- src/multithreading/java/code/day06August2026/Main.java
+++ src/multithreading/java/code/day06August2026/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        NumberThread nt = new NumberThread();
+        nt.start();
 
+        for(int i =1; i<= 10; i++){
+            System.out.println("Main Thread: ");
+        }
         //end timer
         long endTimer = System.nanoTime();
 
