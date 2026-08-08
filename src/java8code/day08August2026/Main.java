23678fc27aac421fc1eb7ed2f9925258d5252c8b
kundan kumar <kundankumaracc20@gmail.com>
2026-08-08 21:15:34 +0530

    August:08::2026::21:12:PM

--- src/java8code/day08August2026/Main.java
+++ src/java8code/day08August2026/Main.java
@@ -5,6 +5,7 @@ import java8code.day06August2026.Employee;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -12,17 +13,23 @@ public class Main {
         //start timer
         long startTimer = System.nanoTime();
 
-        List<Employees> empList = Arrays.asList(
-                new Employees(100, "kundan kumar",1000000),
-                new Employees(101, "kumar",80000),
-                new Employees(102, "prajapati",90000),
-                new Employees(104,"kunal",850000)
-        );
+//        List<Employees> empList = Arrays.asList(
+//                new Employees(100, "kundan kumar",1000000),
+//                new Employees(101, "kumar",80000),
+//                new Employees(102, "prajapati",90000),
+//                new Employees(104,"kunal",850000)
+//        );
+//
+//        Map<Boolean,List<Employees>> finalList= empList.stream()
+//                .collect(Collectors.partitioningBy(e ->e.salary()>85000));
+//
+//        System.out.println(finalList);
 
-        Map<Boolean,List<Employees>> finalList= empList.stream()
-                .collect(Collectors.partitioningBy(e ->e.salary()>85000));
+        List<Integer> numbers = Arrays.asList(1,82,3,9,4,12,90,44,1,3,3);
 
-        System.out.println(finalList);
+        Set<Integer> finalResult = numbers.stream().collect(Collectors.toSet());
+
+        System.out.println(finalResult);
 
         //end timer
         long endTimer = System.nanoTime();
