af432fe36e763724102cee18d454efb13362d55b
kundan kumar <kundankumaracc20@gmail.com>
2026-08-09 19:54:31 +0530

    August:09::2026::19:54:PM

---
+++ src/multithreading/java/code/day09August2026/Main.java
@@ -0,0 +1,19 @@
+package multithreading.java.code.day09August2026;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+        //start timer
+        long startTimer = System.nanoTime();
+        for(int i =1; i <= 5; i++){
+            System.out.println(i);
+            Thread.sleep(2000);
+        }
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer - startTimer)/1_000_000;
+
+        System.out.println("Program time: "+programTime+ " ms");
+    }
+}
