0353e60a9bf5a0508cbbe5a1fe1f6202cd265ef6
kundan kumar <kundankumaracc20@gmail.com>
2026-08-10 09:28:07 +0530

    August:10::2026::09:28:AM

--- src/java8code/day10August2026/Main.java
+++ src/java8code/day10August2026/Main.java
@@ -3,6 +3,7 @@ package java8code.day10August2026;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -14,12 +15,28 @@ public class Main {
 
         List<String> wordList = Arrays.asList("java","Docker","java","kubernetes","spring","spring");
 
-        Map<String,Long> wordFrequency = wordList.stream()
+//        Map<String,Long> wordFrequency = wordList.stream()
+//                .collect(Collectors.groupingBy(
+//                        Function.identity(),
+//                        Collectors.counting()
+//                ));
+//        System.out.println(wordFrequency);
+
+//        Map<Integer,Long> similarWordCount = wordList.stream()
+//                .collect(Collectors.groupingBy(
+//                        String::length,
+//                        Collectors.counting()
+//                ));
+//        System.out.println(similarWordCount);
+
+        Map<Integer, Set<String>> groupByWordLength = wordList.stream()
                 .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()
+                        String::length,
+                        Collectors.toSet()
                 ));
-        System.out.println(wordFrequency);
+
+        System.out.println(groupByWordLength);
+
         //end timer
         long endTimer = System.nanoTime();
 
