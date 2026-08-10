9d8d109208cd46b71ee4ff0c662d456a285594b0
kundan kumar <kundankumaracc20@gmail.com>
2026-08-10 09:10:03 +0530

    August:10::2026::09:10:AM

---
+++ src/java8code/day10August2026/Main.java
@@ -0,0 +1,31 @@
+package java8code.day10August2026;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+//         Frequency of Every Word
+
+        List<String> wordList = Arrays.asList("java","Docker","java","kubernetes","spring","spring");
+
+        Map<String,Long> wordFrequency = wordList.stream()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
+        System.out.println(wordFrequency);
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer - startTimer)/1_000_000;
+
+        System.out.println("program time: "+programTime+ " ms");
+    }
+}
