25fa49f86181a776dc7b69a0eb9749a1ce462682
kundan kumar <kundankumaracc20@gmail.com>
2026-08-12 09:27:04 +0530

    August:12::2026::09:27:AM

--- src/java8code/day12August2026/Main.java
+++ src/java8code/day12August2026/Main.java
@@ -1,7 +1,9 @@
 package java8code.day12August2026;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -9,24 +11,37 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-//         Find First Repeated Character
-        String word = "Programming";
+////         Find First Repeated Character
+//        String word = "Programming";
+//
+//        Map<Character,Long> wordFrequency = word.chars()
+//                .mapToObj(i -> (char)i)
+//                .collect(Collectors.groupingBy(
+//                        Function.identity(),
+//                        Collectors.counting()
+//                ));
+//
+//        System.out.println(wordFrequency);
+//
+//        Optional<Character> firstRepeatedCharacter = wordFrequency.entrySet()
+//                .stream()
+//                .filter(e -> e.getValue() != 1)
+//                .map(Map.Entry::getKey)
+//                .findFirst();
+//        System.out.println(firstRepeatedCharacter.orElse(null));
 
-        Map<Character,Long> wordFrequency = word.chars()
-                .mapToObj(i -> (char)i)
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()
-                ));
+//        String word = "microservices";
+//
+//        Set<Character> seen = new HashSet<>();
+//
+//        Optional<Character> firstRepetedCharacter = word.chars()
+//                .mapToObj(i -> (char)i)
+//                .filter(i -> !seen.add(i))
+//                .findFirst();
+//
+//        System.out.println(firstRepetedCharacter.orElse(null));
 
-        System.out.println(wordFrequency);
 
-        Optional<Character> firstRepeatedCharacter = wordFrequency.entrySet()
-                .stream()
-                .filter(e -> e.getValue() != 1)
-                .map(Map.Entry::getKey)
-                .findFirst();
-        System.out.println(firstRepeatedCharacter);
         //end timer
         long endTimer = System.nanoTime();
 
