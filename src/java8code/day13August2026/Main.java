222b18440f838940fe5add750034e200859bf921
kundan kumar <kundankumaracc20@gmail.com>
2026-08-13 09:10:41 +0530

    August:13::2026::09:10:AM

--- src/java8code/day13August2026/Main.java
+++ src/java8code/day13August2026/Main.java
@@ -1,23 +1,34 @@
 package java8code.day13August2026;
 
 import java.lang.classfile.Opcode;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-//      Find Common Elements Between Two Lists
-        List<Integer> firstList = Arrays.asList(1,2,5,9,44,11,3,12,51,10);
-        List<Integer> secondList = Arrays.asList(10,21,11,13,17,44,51,9999);
+////      Find Common Elements Between Two Lists
+//        List<Integer> firstList = Arrays.asList(1,2,5,9,44,11,3,12,51,10);
+//        List<Integer> secondList = Arrays.asList(10,21,11,13,17,44,51,9999);
+//
+//        List<Integer> commonList = firstList.stream()
+//                .filter(secondList::contains)
+//                .toList();
+//
+//        System.out.println(commonList);
 
-        List<Integer> commonList = firstList.stream()
-                .filter(secondList::contains)
-                .toList();
+//        Merge Two Lists Without Duplicates
+        List<Integer> firstList = Arrays.asList(1,2,3,4,1,8,9);
+        List<Integer> secondList = Arrays.asList(1,2,7,10,11,12,16,18);
 
-        System.out.println(commonList);
+        Set<Integer> finalListWithoutDuplicate = Stream
+                .concat(firstList.stream(),secondList.stream())
+                .collect(Collectors.toSet());
+
+
+        System.out.println(finalListWithoutDuplicate);
 
         //end timer
         long endTimer = System.nanoTime();
