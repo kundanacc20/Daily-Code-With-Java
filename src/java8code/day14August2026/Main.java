87b80a1592d4bcac612e8bb338bb2eff47961b42
kundan kumar <kundankumaracc20@gmail.com>
2026-08-14 09:38:19 +0530

    August:14::2026::09:38:AM

--- src/java8code/day14August2026/Main.java
+++ src/java8code/day14August2026/Main.java
@@ -7,11 +7,17 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        List<Integer> intList = Arrays.asList(1,3,8,11,7,5);
+//        List<Integer> intList = Arrays.asList(1,3,8,11,7,5);
+//
+//        int sum = intList.stream().reduce(0,Integer::sum);
+//
+//        System.out.println(sum);
 
-        int sum = intList.stream().reduce(0,Integer::sum);
+        //string concatination
+        List<String> wordList = Arrays.asList("kundan"," kumar", " java "," developer ");
 
-        System.out.println(sum);
+        String finalWord = wordList.stream().reduce("",String::concat);
+        System.out.println(finalWord);
         //end timer
         long endTimer = System.nanoTime();
 
