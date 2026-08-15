bd105b04f83ad0ff2edef0669c91ee8dadb2fee6
kundan kumar <kundankumaracc20@gmail.com>
2026-08-15 12:09:08 +0530

    August:15::2026::12:09:PM

--- src/java8code/day15August2026/flatmapusecase/Order.java
+++ src/java8code/day15August2026/flatmapusecase/Order.java
@@ -1,4 +1,15 @@
 package java8code.day15August2026.flatmapusecase;
 
+import java.util.List;
+
 public class Order {
+    List<Item> items;
+
+    public Order(List<Item> items){
+        this.items = items;
+    }
+
+    List<Item> getItems() {
+        return items;
+    }
 }
