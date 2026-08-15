74898791203738b351f13f599f994e7aa5e31dd9
kundan kumar <kundankumaracc20@gmail.com>
2026-08-15 12:19:04 +0530

    August:15::2026::12:19:PM

--- src/java8code/day15August2026/flatmapusecase/Main.java
+++ src/java8code/day15August2026/flatmapusecase/Main.java
@@ -1,10 +1,30 @@
 package java8code.day15August2026.flatmapusecase;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        //sample data
+        Customer c1 = new Customer(Arrays.asList(
+                new Order(Arrays.asList(new Item("Laptop"),new Item("Mouse"))),
+                new Order(Arrays.asList(new Item("keyboard")))
+        ));
 
+        Customer c2 = new Customer(Arrays.asList(
+                new Order(Arrays.asList(new Item("Phone"), new Item("charger")))
+        ));
+        List<Customer> customers = Arrays.asList(c1, c2);
+
+        List<Item> allItems = customers.stream()
+                .flatMap(c->c.getOrders().stream())
+                .flatMap(o -> o.getItems().stream())
+                .collect(Collectors.toList());
+
+        System.out.println(allItems);
         //end timer
         long endTimer = System.nanoTime();
 
