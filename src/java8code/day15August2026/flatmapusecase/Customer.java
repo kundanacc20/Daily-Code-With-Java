28241d64fcd0f095568b44f7b741d3fb4f8a407c
kundan kumar <kundankumaracc20@gmail.com>
2026-08-15 12:11:48 +0530

    August:15::2026::12:11:PM

--- src/java8code/day15August2026/flatmapusecase/Customer.java
+++ src/java8code/day15August2026/flatmapusecase/Customer.java
@@ -1,4 +1,13 @@
 package java8code.day15August2026.flatmapusecase;
 
+import java.util.List;
+
 public class Customer {
+    List<Order> orders;
+    public Customer(List<Order> orders){
+        this.orders = orders;
+    }
+    List<Order> getOrders(){
+        return orders;
+    }
 }
