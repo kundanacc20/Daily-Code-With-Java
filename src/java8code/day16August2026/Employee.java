6cb5f1208744c422af8ab8eb90c66d74e29318d0
kundan kumar <kundankumaracc20@gmail.com>
2026-08-16 10:30:04 +0530

    August:16::2026::10:30:AM

---
+++ src/java8code/day16August2026/Employee.java
@@ -0,0 +1,15 @@
+package java8code.day16August2026;
+
+import java.util.Date;
+
+public record Employee(int id, String name, String department, String gender, int salary, Date joiningYear, String city) {
+}
+
+//Create an Employee class with:
+//id
+//name
+//department
+//gender
+//salary
+//joiningYear
+//city
