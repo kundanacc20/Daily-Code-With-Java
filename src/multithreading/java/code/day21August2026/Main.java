dab53fc99f96fce73795f965b26b37cd0f4e96de
kundan kumar <kundankumaracc20@gmail.com>
2026-08-21 07:01:21 +0530

    August:21::2026::07:01:AM

--- src/multithreading/java/code/day21August2026/Main.java
+++ src/multithreading/java/code/day21August2026/Main.java
@@ -1,10 +1,40 @@
 package multithreading.java.code.day21August2026;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         //start timer
         long startTimer = System.nanoTime();
 
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(()->{
+            for (int i = 1; i < 100000; i++)
+                counter.increment();
+        });
+
+        Thread t2 = new Thread(() ->{
+            for (int i = 1; i<100000; i++){
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(counter.count);
+/*
+here the expected output is the 200000 but generally it does not
+touches that because of race condition.
+
+Concept:
+A race condition happens when multiple threads access and modify
+shared data concurrently without proper synchronization.
+Because the increment operation (counter++) is not atomic,
+updates can be lost.
+ */
         //end timer
         long endTimer = System.nanoTime();
 
