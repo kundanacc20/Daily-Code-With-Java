0889bd946f37b7f2ea52f13a2b4acd7d379e46ee
kundan kumar <kundankumaracc20@gmail.com>
2026-08-21 07:24:28 +0530

    August:21::2026::07:24:AM

--- src/multithreading/java/code/day21August2026/Main.java
+++ src/multithreading/java/code/day21August2026/Main.java
@@ -5,26 +5,26 @@ public class Main {
         //start timer
         long startTimer = System.nanoTime();
 
-        Counter counter = new Counter();
-
-        Thread t1 = new Thread(()->{
-            for (int i = 1; i < 100000; i++)
-                counter.increment();
-        });
-
-        Thread t2 = new Thread(() ->{
-            for (int i = 1; i<100000; i++){
-                counter.increment();
-            }
-        });
-
-        t1.start();
-        t2.start();
-
-        t1.join();
-        t2.join();
-
-        System.out.println(counter.count);
+//        Counter counter = new Counter();
+//
+//        Thread t1 = new Thread(()->{
+//            for (int i = 1; i < 100000; i++)
+//                counter.increment();
+//        });
+//
+//        Thread t2 = new Thread(() ->{
+//            for (int i = 1; i<100000; i++){
+//                counter.increment();
+//            }
+//        });
+//
+//        t1.start();
+//        t2.start();
+//
+//        t1.join();
+//        t2.join();
+//
+//        System.out.println(counter.count);
 /*
 here the expected output is the 200000 but generally it does not
 touches that because of race condition.
@@ -35,6 +35,27 @@ shared data concurrently without proper synchronization.
 Because the increment operation (counter++) is not atomic,
 updates can be lost.
  */
+
+        SynchronizedCounter synchronizedCounter = new SynchronizedCounter();
+
+        Thread thread1 = new Thread(()->{
+            for (int i =0; i<10000000; i++)
+                synchronizedCounter.increment();
+        });
+
+        Thread thread2 = new Thread(() ->{
+            for (int i = 1; i <= 10000000; i++){
+                synchronizedCounter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(synchronizedCounter.count);
         //end timer
         long endTimer = System.nanoTime();
 
