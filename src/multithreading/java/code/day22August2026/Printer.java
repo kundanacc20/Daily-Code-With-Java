d6d299e273ff4e758e0944e003d584510b3ff028
kundan kumar <kundankumaracc20@gmail.com>
2026-08-22 15:44:21 +0530

    August:22::2026::Dambledor

--- src/multithreading/java/code/day22August2026/Printer.java
+++ src/multithreading/java/code/day22August2026/Printer.java
@@ -2,12 +2,25 @@ package multithreading.java.code.day22August2026;
 
 public class Printer {
 
-    synchronized static void print(String name){
-        for(int i =1; i<= 5; i++){
-            System.out.println(name+" "+i);
+//    synchronized static void print(String name){
+//        for(int i =1; i<= 5; i++){
+//            System.out.println(name+" "+i);
+//            try {
+//                Thread.sleep(100);
+//            }catch (InterruptedException e){
+//                Thread.currentThread().interrupt();
+//            }
+//        }
+//    }
+
+    synchronized void print(){
+        for (int i =1; i<=5; i++){
+            System.out.println(Thread.currentThread().getName()
+            +" : "+i);
+
             try {
                 Thread.sleep(100);
-            }catch (InterruptedException e){
+            } catch (InterruptedException e){
                 Thread.currentThread().interrupt();
             }
         }
