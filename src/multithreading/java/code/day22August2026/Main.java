c489fc412f1bd2fe6df1b66675cfe562d339ce11
kundan kumar <kundankumaracc20@gmail.com>
2026-08-22 15:12:27 +0530

    August:22::2026::saturday:codefun

--- src/multithreading/java/code/day22August2026/Main.java
+++ src/multithreading/java/code/day22August2026/Main.java
@@ -1,10 +1,30 @@
 package multithreading.java.code.day22August2026;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         //start timer
         long startTimer = System.nanoTime();
+        BankAccount account = new BankAccount();
 
+        Thread t1 = new Thread(()->{
+            for(int i =1; i<= 1000; i++){
+                account.deposit(100);
+            }
+        });
+
+        Thread t2 = new Thread(() ->{
+            for (int i = 1; i <= 1000; i++){
+                account.deposit(100);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Balance: "+account.getBalance());
         //end timer
         long endTimer = System.nanoTime();
 
