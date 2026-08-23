86ae9b46b453cc3cb08a705a8429da6b83479300
kundan kumar <kundankumaracc20@gmail.com>
2026-08-23 13:31:04 +0530

    August:23:2026::Courage, bravery, chivalry, determination are the key to greathuman

--- src/multithreading/java/code/day23August2026/Main.java
+++ src/multithreading/java/code/day23August2026/Main.java
@@ -1,9 +1,20 @@
 package multithreading.java.code.day23August2026;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         //start timer
         long startTimer = System.nanoTime();
+        Task task1 = new Task();
+        Task task2 = new Task();
+
+        Thread t1 = new Thread(Task::execute,"Thread-1");
+        Thread t2 = new Thread(Task::execute,"Thread-2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
 
         //end timer
         long endTimer = System.nanoTime();
