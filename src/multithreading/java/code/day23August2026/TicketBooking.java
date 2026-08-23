c2bf4a47c86619ecf36ee2c7b9f0a76460013799
kundan kumar <kundankumaracc20@gmail.com>
2026-08-23 14:10:02 +0530

    August:23:2026::Godric's Hollow

---
+++ src/multithreading/java/code/day23August2026/TicketBooking.java
@@ -0,0 +1,18 @@
+package multithreading.java.code.day23August2026;
+
+public class TicketBooking {
+    private int availableTickets = 5;
+
+    synchronized void bookTicket(String user, int tickets){
+        if(availableTickets >= tickets){
+            System.out.println(user+ " is booking "+tickets+ "ticket(s)");
+
+            availableTickets -= tickets;
+            System.out.println(
+                    "tickets remaining: "+availableTickets
+            );
+        } else {
+            System.out.println(user+ " not enough tickets");
+        }
+    }
+}
