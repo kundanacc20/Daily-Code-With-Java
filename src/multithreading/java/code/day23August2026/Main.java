607f583df0fabe46e04d85b40c3ddba1a545fd65
kundan kumar <kundankumaracc20@gmail.com>
2026-08-23 13:57:40 +0530

    August:23:2026::Do what is right, even when it is difficult

--- src/multithreading/java/code/day23August2026/Main.java
+++ src/multithreading/java/code/day23August2026/Main.java
@@ -4,17 +4,35 @@ public class Main {
     public static void main(String[] args) throws InterruptedException{
         //start timer
         long startTimer = System.nanoTime();
-        Task task1 = new Task();
-        Task task2 = new Task();
+//        Task task1 = new Task();
+//        Task task2 = new Task();
+//
+//        Thread t1 = new Thread(Task::execute,"Thread-1");
+//        Thread t2 = new Thread(Task::execute,"Thread-2");
+//
+//        t1.start();
+//        t2.start();
+//
+//        t1.join();
+//        t2.join();
 
-        Thread t1 = new Thread(Task::execute,"Thread-1");
-        Thread t2 = new Thread(Task::execute,"Thread-2");
+        Counter counter = new Counter();
 
-        t1.start();
-        t2.start();
+        Thread[] threads = new Thread[10];
 
-        t1.join();
-        t2.join();
+        for (int i = 0; i< 10; i++){
+            threads[i] = new Thread(() ->{
+                for (int j = 0; j < 10000; j++){
+                    counter.increment();
+                }
+            });
+            threads[i].start();
+        }
+
+        for(Thread thread: threads){
+            thread.join();
+        }
+        System.out.println("final count : "+counter.getCount());
 
         //end timer
         long endTimer = System.nanoTime();
