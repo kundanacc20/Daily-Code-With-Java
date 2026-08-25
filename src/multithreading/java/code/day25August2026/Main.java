66b28cdfdaf14cd20a407c97e8ade2eadb4631d7
kundan kumar <kundankumaracc20@gmail.com>
2026-08-25 07:32:41 +0530

    august:25:2026::help will be come on the time of need

--- src/multithreading/java/code/day25August2026/Main.java
+++ src/multithreading/java/code/day25August2026/Main.java
@@ -1,9 +1,32 @@
 package multithreading.java.code.day25August2026;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+//        Problem 1: Find duplicate elements
+//
+//        Input: [10, 20, 30, 20, 40, 10, 50]
+//        Output: [10, 20]
+
+//        List<Integer> arrayList = Arrays.asList(10,20,30,20,40,10,50);
+//
+//        List<Integer> duplicateElement = arrayList.stream().collect(Collectors.groupingBy(
+//                Function.identity(),
+//                Collectors.counting()
+//        ))
+//                .entrySet()
+//                .stream()
+//                .filter(e -> e.getValue()>1)
+//                .map(Map.Entry::getKey)
+//                .collect(Collectors.toList());
+//        System.out.println(duplicateElement);
 
         //end timer
         long endTimer = System.nanoTime();
