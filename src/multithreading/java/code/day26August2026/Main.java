f69e74fca40ed705516e6c29d393c9b288e85179
kundan kumar <kundankumaracc20@gmail.com>
2026-08-26 07:35:10 +0530

    August:26:2026::The International Statute of Wizarding Secrecy helps keep magic hidden from Muggles.

--- src/multithreading/java/code/day26August2026/Main.java
+++ src/multithreading/java/code/day26August2026/Main.java
@@ -2,6 +2,7 @@ package multithreading.java.code.day26August2026;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -18,12 +19,17 @@ Concepts: TreeSet, sorting.
  */
         List<Integer> numberList = Arrays.asList(10,50,20,80,11,30,99);
 
-        List<Integer> secondLargestNumber = numberList.stream()
-                .sorted((a,b)->b -a)
-                .skip(1)
-                .collect(Collectors.toList());
-        System.out.println(secondLargestNumber);
-        System.out.println(secondLargestNumber.getFirst());
+//        List<Integer> secondLargestNumber = numberList.stream()
+//                .sorted((a,b)->b -a)
+//                .skip(1)
+//                .collect(Collectors.toList());
+//        System.out.println(secondLargestNumber);
+//        System.out.println(secondLargestNumber.getFirst());
+
+        TreeSet<Integer> treeSet = new TreeSet<>(numberList);
+        System.out.println(treeSet.pollLast());
+        System.out.println(treeSet);
+        System.out.println(treeSet.pollLast());
 
         //end timer
         long endTimer = System.nanoTime();
