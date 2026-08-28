45d79a094c87b22f9686d7e398454a935ae21da0
kundan kumar <kundankumaracc20@gmail.com>
2026-08-28 09:51:27 +0530

    august:28:2026::House colors in the books: Blue and bronze

--- src/javacollection/code/day28august2026/Main.java
+++ src/javacollection/code/day28august2026/Main.java
@@ -15,7 +15,7 @@ public class Main {
         [30, 40]
         Concept: HashSet
          */
-        List<Integer> list1 = Arrays.asList(1,10,20,30,40);
+       /* List<Integer> list1 = Arrays.asList(1,10,20,30,40);
         List<Integer> list2 = Arrays.asList(1,30,40,50,60);
 
 //        Set<Integer> commonElement = new HashSet<>();
@@ -38,6 +38,25 @@ public class Main {
             }
         }
         System.out.println(common);
+        */
+/*Problem 7: Find union of two lists
+Input:
+[1, 2, 3, 4]
+[3, 4, 5, 6]
+
+Output:
+[1, 2, 3, 4, 5, 6]
+Concept: Set
+*/
+        /*List<Integer> list1 = Arrays.asList(1,2,3,4,5);
+        List<Integer> list2 = Arrays.asList(6,7,8,9,10);
+
+        Set<Integer> unionOfList = new HashSet<>(list1);
+        for(Integer e : list2){
+            unionOfList.add(e);
+        }
+        System.out.println(unionOfList);
+         */
         //end timer
         long endTimer = System.nanoTime();
 
