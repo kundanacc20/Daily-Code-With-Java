112e2dd7bc8b661f9f61fe2aad0209d2c760f267
kundan kumar <kundankumaracc20@gmail.com>
2026-08-28 09:32:27 +0530

    August:28:2026::Founder is Rowena Ravenclaw

--- src/javacollection/code/day28august2026/Main.java
+++ src/javacollection/code/day28august2026/Main.java
@@ -1,10 +1,35 @@
 package javacollection.code.day28august2026;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        /*Problem 6: Find common elements between two lists
+        Input:
+        List 1 = [10, 20, 30, 40]
+        List 2 = [30, 40, 50, 60]
 
+        Output:
+        [30, 40]
+        Concept: HashSet
+         */
+        List<Integer> list1 = Arrays.asList(10,20,30,40);
+        List<Integer> list2 = Arrays.asList(30,40,50,60);
+
+        Set<Integer> commonElement = new HashSet<>();
+
+        for(Integer e : list2)
+        {
+            if(list1.contains(e)){
+                commonElement.add(e);
+            }
+        }
+        System.out.println(commonElement);
         //end timer
         long endTimer = System.nanoTime();
 
