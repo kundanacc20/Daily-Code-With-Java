321818bce902bcc1fe3fea772813dcaf21489ea5
kundan kumar <kundankumaracc20@gmail.com>
2026-08-30 21:45:59 +0530

    💛 House Colors: Yellow and black.

--- src/corejava/day30August2026/Main.java
+++ src/corejava/day30August2026/Main.java
@@ -4,7 +4,19 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Day today = Day.MONDAY;
 
+        if(today == Day.SATURDAY || today == Day.SUNDAY){
+            System.out.println("today is weekend");
+        } else {
+            System.out.println("today is weekdays");
+        }
+
+        //iterating over enum
+
+        for(Day d: Day.values()){
+            System.out.println(d);
+        }
         //end timer
         long endTimer = System.nanoTime();
 
