826748b6e00df325f622fe0127b2310af4682e50
kundan kumar <kundankumaracc20@gmail.com>
2026-09-04 09:15:44 +0530

    Its motto is “Fortune favors the bold” — emphasizing courage and ambition.

--- src/corejava/day4september2026/Main.java
+++ src/corejava/day4september2026/Main.java
@@ -4,7 +4,21 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Message message = new Message();
 
+        Thread producer = new Thread(()->{
+            for (int i = 1; i <= 5; i++){
+                message.produce("Message : "+i);
+            }
+        });
+
+        Thread consumer = new Thread(()->{
+            for (int i = 1; i<=5; i++){
+                message.consume();
+            }
+        });
+        producer.start();
+        consumer.start();
         //end timer
         long endTimer = System.nanoTime();
 
