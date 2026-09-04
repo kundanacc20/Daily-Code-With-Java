06d4450e768fe77dfa71c2d964e7cec3346eb3e3
kundan kumar <kundankumaracc20@gmail.com>
2026-09-04 09:41:19 +0530

    The vaults are protected by powerful magical security systems, including enchantments, locks, and guards.

--- src/corejava/day4september2026/Main.java
+++ src/corejava/day4september2026/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        Message message = new Message();
+        /*Message message = new Message();
 
         Thread producer = new Thread(()->{
             for (int i = 1; i <= 5; i++){
@@ -19,6 +19,28 @@ public class Main {
         });
         producer.start();
         consumer.start();
+         */
+
+        /*SharedResource resource = new SharedResource();
+
+        Thread c1 = new Thread(()->resource.consume("Consumer-1"));
+        Thread c2 = new Thread(()->resource.consume("consumer-2"));
+        Thread c3 = new Thread(() ->resource.consume("consumer-3"));
+
+        c1.start();
+        c2.start();
+        c3.start();
+
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+
+        Thread producer = new Thread(resource::produce);
+        producer.start();
+
+         */
         //end timer
         long endTimer = System.nanoTime();
 
