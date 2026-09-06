caebe6b1e93906857b4bba0fcecf8f8de1be60c0
kundan kumar <kundankumaracc20@gmail.com>
2026-09-06 20:12:41 +0530

    Sacred Twenty-Eight :-The Weasleys were listed among the oldest pure-blood families but rejected elitism, embracing equality with Muggle-borns.

--- src/graphDS/day6september2026/GraphUtils.java
+++ src/graphDS/day6september2026/GraphUtils.java
@@ -1,4 +1,19 @@
 package graphDS.day6september2026;
 
+import java.util.List;
+
 public class GraphUtils {
+    public static void printGraph(GraphList graph){
+        List<List<Integer>> adjList = graph.getAdjList();
+        for(int i =0; i < graph.getVertices(); i++){
+            System.out.print("Vertex "+i+" : ");
+            for(int neighbor : adjList.get(i)){
+                System.out.print(neighbor+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static boolean hasEdge(GraphList graph, int src, int dest){
+        return graph.getAdjList().get(src).contains(dest);
+    }
 }
