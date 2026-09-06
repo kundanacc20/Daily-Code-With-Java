a333d27fa85ebfa29a158f5f84beca488f4e5cdf
kundan kumar <kundankumaracc20@gmail.com>
2026-09-06 20:16:21 +0530

    Order of the Phoenix  :-Every member fought against Voldemort in the Second Wizarding War, showing immense bravery.

--- src/graphDS/day6september2026/Main.java
+++ src/graphDS/day6september2026/Main.java
@@ -4,7 +4,19 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        GraphList g = new GraphList(5);
 
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+
+        GraphUtils.printGraph(g);
+
+        System.out.println("Edge between 0 and 2? " + GraphUtils.hasEdge(g, 0, 2));
         //end timer
         long endTimer = System.nanoTime();
 
