61c3407f450022ac9afea94e739431f915dfc885
kundan kumar <kundankumaracc20@gmail.com>
2026-09-07 09:19:46 +0530

    Departments:->(1)Magical Law Enforcement(2)Department of Mysteries(3)Magical Transportation(4)International Magical Cooperation(5)Magical Games and Sports

--- src/graphDS/day7september2026/Main.java
+++ src/graphDS/day7september2026/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        WeightedGraph g = new WeightedGraph(5,false);
+        g.addEdge(0, 1, 4);
+        g.addEdge(0, 2, 1);
+        g.addEdge(2, 1, 2);
+        g.addEdge(1, 3, 1);
+        g.addEdge(2, 3, 5);
+        g.addEdge(3, 4, 3);
 
+        GraphUtils.printGraph(g);
         //end timer
         long endTimer = System.nanoTime();
 
