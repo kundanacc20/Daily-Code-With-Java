4ff22e270779f9d1e453d967cc5be5152f32d063
kundan kumar <kundankumaracc20@gmail.com>
2026-09-08 09:44:18 +0530

    Salazar Slytherin’s locket	A Muggle tramp	Ron Weasley (Sword of Gryffindor)

--- src/corejavafeatures/Main.java
+++ src/corejavafeatures/Main.java
@@ -1,5 +1,8 @@
 package corejavafeatures;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         //start timer
@@ -8,9 +11,17 @@ public class Main {
 //
 //        System.out.println(s.calculateArea(5));
 
-        //abstract class and abstract method
-        Shape s = new Triangle();
-        s.draw();
+//        //abstract class and abstract method
+//        Shape s = new Triangle();
+//        s.draw();
+        //static method reference
+        Arrays.asList(2,3,4,5,6,7,8)
+                .forEach(n->System.out.print(Math.sqrt(n)+" "));
+
+        Arrays.asList(1,2,3,4,5,6,9)
+                .stream()
+                .map(Math::sqrt)
+                .forEach(System.out::println);
         //end timer
         long endTimer = System.nanoTime();
 
