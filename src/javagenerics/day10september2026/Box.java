d38e4616f7bc238511310eb33adf79cf1c0707a3
kundan kumar <kundankumaracc20@gmail.com>
2026-09-10 10:03:18 +0530

    The Order of the Phoenix was a secret society founded by Albus Dumbledore to fight Lord Voldemort and his Death Eaters, first formed in the 1970s and revived in 1995 after Voldemort’s return. Its headquarters was at Number 12, Grimmauld Place, and it played a crucial role in both Wizarding Wars.

---
+++ src/javagenerics/day10september2026/Box.java
@@ -0,0 +1,13 @@
+package javagenerics.day10september2026;
+
+public class Box<T> {
+    private T value;
+
+    public void setValue(T value){
+        this.value = value;
+    }
+
+    public T getValue(){
+        return value;
+    }
+}
