245f0cc298267372b096ee6c41c8dd4032b07ea0
kundan kumar <kundankumaracc20@gmail.com>
2026-09-12 10:38:47 +0530

    sept:12:2026::today we talk about Quidditch in the Harry Potter world

---
+++ src/javagenerics/day12september2026/Main.java
@@ -0,0 +1,18 @@
+package javagenerics.day12september2026;
+
+public class Main {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+        Pair<String,Integer> stringIntegerPair = new Pair<>("kundan",27);
+
+        System.out.println(stringIntegerPair);
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer - startTimer)/1_000_000;
+
+        System.out.println("program time: "+programTime+" ms");
+    }
+}
