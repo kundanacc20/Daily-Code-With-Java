9538202d0251024498670a6417f19565dfd0596c
kundan kumar <kundankumaracc20@gmail.com>
2026-09-12 10:41:26 +0530

    Quidditch in the Harry Potter books is a magical sport played on broomsticks with seven players per team, featuring the Quaffle, Bludgers, and the Golden Snitch. A quirky fact: there are 700 possible fouls in Quidditch, and all of them were committed during the infamous 1473 Quidditch World Cup!

--- src/javagenerics/day12september2026/Main.java
+++ src/javagenerics/day12september2026/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        Pair<String,Integer> stringIntegerPair = new Pair<>("kundan",27);
-
-        System.out.println(stringIntegerPair);
+//        Pair<String,Integer> stringIntegerPair = new Pair<>("kundan",27);
+//
+//        System.out.println(stringIntegerPair);
+//        // string , string
+//        Pair<String,String> stringStringPair = new Pair<>("kundan","kumar");
+//        System.out.println(stringStringPair);
         //end timer
         long endTimer = System.nanoTime();
 
