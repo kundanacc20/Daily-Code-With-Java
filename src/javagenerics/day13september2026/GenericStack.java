375cd1cd6588e3665efb486df0082ad75e6a133f
kundan kumar <kundankumaracc20@gmail.com>
2026-09-13 11:13:49 +0530

    sept:13:2026::Fouls: There are 700 fouls, ranging from “blatching” (flying with elbows out to hurt opponents) to “stooging” (two Chasers entering the scoring area together).

---
+++ src/javagenerics/day13september2026/GenericStack.java
@@ -0,0 +1,4 @@
+package javagenerics.day13september2026;
+
+public class GenericStack {
+}
---
+++ src/javagenerics/day13september2026/Main.java
@@ -0,0 +1,14 @@
+package javagenerics.day13september2026;
+
+public class Main {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer - startTimer)/1_000_000;
+    }
+}
