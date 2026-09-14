ab11a41675c2f84f7e6abd0925ed1ad95a68c65e
kundan kumar <kundankumaracc20@gmail.com>
2026-09-14 08:48:33 +0530

    Diagon Alley is the bustling wizarding marketplace hidden behind the Leaky Cauldron in London, where Hogwarts students and witches/wizards shop for magical supplies like wands, robes, books, and even pets. It is also home to Gringotts, the only wizarding bank, run by goblins.

--- src/javagenerics/day14september2026/WildcardSum.java
+++ src/javagenerics/day14september2026/WildcardSum.java
@@ -1,4 +1,13 @@
 package javagenerics.day14september2026;
 
+import java.util.List;
+
 public class WildcardSum {
+    public static double sum(List<? extends Number> list){
+        double sumTotal = 0;
+        for(Number n : list){
+            sumTotal += n.doubleValue();
+        }
+        return sumTotal;
+    }
 }
