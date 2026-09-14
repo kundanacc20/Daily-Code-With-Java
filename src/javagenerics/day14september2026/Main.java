e329164cd44402d8aee33546180658a7378c472e
kundan kumar <kundankumaracc20@gmail.com>
2026-09-14 08:57:23 +0530

    Location: Situated behind the Leaky Cauldron on Charing Cross Road, London. Muggles cannot see it; entry requires tapping a specific brick in the courtyard wall.

--- src/javagenerics/day14september2026/Main.java
+++ src/javagenerics/day14september2026/Main.java
@@ -1,10 +1,16 @@
 package javagenerics.day14september2026;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        List<Integer> integerList = List.of(1,2,3,4,5,6,7,8);
+        System.out.println(WildcardSum.sum(integerList));
 
+        List<Double> doubleList = List.of(1.1,2.1,3.1,9.9);
+        System.out.println(String.format("%.2f",WildcardSum.sum(doubleList)));
         //end timer
         long endTimer = System.nanoTime();
 
