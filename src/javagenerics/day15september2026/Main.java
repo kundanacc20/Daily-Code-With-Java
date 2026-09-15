646b9877910aa03776b6bed394c64b0cb0ff5fcc
kundan kumar <kundankumaracc20@gmail.com>
2026-09-15 08:44:26 +0530

    Flourish & Blotts – Bookstore selling Hogwarts textbooks.

--- src/javagenerics/day15september2026/Main.java
+++ src/javagenerics/day15september2026/Main.java
@@ -1,10 +1,18 @@
 package javagenerics.day15september2026;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        List<Integer> src = Arrays.asList(1,2,3,4);
+        List<Number> dest = new ArrayList<>();
 
+        CopyUtil.copy(src,dest);
+        ResultPrinter.print(dest);
         //end timer
         long endTimer = System.nanoTime();
 
