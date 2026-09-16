4c8e44dc8a10b6a6eec930f0dd4a42988b65e9aa
kundan kumar <kundankumaracc20@gmail.com>
2026-09-16 08:33:59 +0530

    Location: Situated in the Highlands of Scotland, overlooking the Black Lake and surrounded by the Forbidden Forest.

--- src/javagenerics/day16september2026/Main.java
+++ src/javagenerics/day16september2026/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        InMemoryRepository<String,Integer> repository = new InMemoryRepository();
+        repository.save(1,"harshit");
+        repository.save(2, "anurag");
 
+        System.out.println(repository.findById(1));
+        repository.deleteById(1);
+        System.out.println(repository.findById(2));
         //end timer
         long endTimer = System.nanoTime();
 
