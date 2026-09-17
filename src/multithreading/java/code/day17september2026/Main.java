394fa586f93e25814681fc9a45f740974d1fa7bf
kundan kumar <kundankumaracc20@gmail.com>
2026-09-17 09:03:45 +0530

    Helga Hufflepuff brought house-elves to work in the kitchens, treating them kindly compared to other wizarding households.

--- src/multithreading/java/code/day17september2026/Main.java
+++ src/multithreading/java/code/day17september2026/Main.java
@@ -4,7 +4,22 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        MyBlockingQueue<Integer> queue = new MyBlockingQueue<>(3);
 
+        Thread producer = new Thread(() ->{
+            for (int i =1; i< 10; i++){
+                queue.put(i);
+            }
+        });
+
+        Thread consumer = new Thread(()->{
+            for (int i = 1; i<=10; i++){
+                queue.take();
+            }
+        });
+
+        producer.start();
+        consumer.start();
         //end timer
         long endTimer = System.nanoTime();
 
