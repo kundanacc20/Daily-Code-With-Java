c0ffd8416ffe1dd3715bbfd25dc9e8c04f7d187b
kundan kumar <kundankumaracc20@gmail.com>
2026-09-20 11:25:07 +0530

    september:20:2026::sunday code day

--- src/multithreading/java/code/day20september2026/ABCPrinter.java
+++ src/multithreading/java/code/day20september2026/ABCPrinter.java
@@ -1,4 +1,47 @@
 package multithreading.java.code.day20september2026;
 
 public class ABCPrinter {
+    private int state = 0;
+
+    public synchronized void printA(){
+        for(int i = 0; i < 5; i++){
+            while (state != 0){
+                waitForTurn();
+            }
+            System.out.println("A");
+
+            state = 1;
+            notifyAll();
+        }
+    }
+
+    public synchronized void printB(){
+        for (int i = 0; i < 5; i++){
+            while (state != 1){
+                waitForTurn();
+            }
+            System.out.println("B");
+            state = 2;
+            notifyAll();
+        }
+    }
+
+    public synchronized void printC(){
+        for (int i =0; i<5;i++){
+            while (state != 2){
+                waitForTurn();
+            }
+            System.out.println("C");
+            state = 0;
+            notifyAll();
+        }
+    }
+
+    private void waitForTurn(){
+        try {
+            wait();
+        } catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+    }
 }
